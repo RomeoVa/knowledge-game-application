714f424d99a46428cdf73bdc744dd981fcfb61b9
Romeo Varela <romeova.7243@gmail.com>
2020-10-29 13:47:13 -0600

    Add Auth react component

--- knowledge-game/src/App.js
+++ knowledge-game/src/App.js
@@ -2,25 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//AWS Amplify
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+import {
+  withAuthenticator , AmplifySignOut
+} from '@aws-amplify/ui-react'
+
+Amplify.configure(awsconfig);
+
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <AmplifySignOut />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
